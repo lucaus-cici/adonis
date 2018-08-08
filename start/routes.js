5635035c1b1658606062319bb98c8f05bec424aa
“wangrui” <pradis@163.com>
2018-08-08 22:24:40 +0800

    first

--- start/routes.js
+++ start/routes.js
@@ -10,9 +10,23 @@
 |
 | A complete guide on routing is available here.
 | http://adonisjs.com/docs/4.1/routing
-|
-*/
-
+|``
+*/ 
 const Route = use('Route')
+const DataBase = use('Database')
 
 Route.on('/').render('welcome')
+// Route.get('/hello', (obj) => {
+//   // 显示查询符的值
+//   const { request } = obj
+//   return `hello ${ request.input('name') }`
+// })
+Route.get('/hello', 'HelloController.render')
+
+/**
+ * async 查询数据表为异步操作
+ * await 等待异步的查询结果
+ */
+Route.get('/post', async () => {
+  return await DataBase.table('posts').select('*')
+})
